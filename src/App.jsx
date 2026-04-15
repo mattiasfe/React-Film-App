2265f02e7969b94269b0c82e2b1d8221b2cd797f
mattiasfe <matferge@gmail.com>
2026-04-15 11:34:17 +0200

    Convert src from submodule to regular folder

---
+++ src/App.jsx
@@ -0,0 +1,109 @@
+
+import React, { use } from 'react'
+import { useEffect, useState } from 'react'
+import Search from './components/Search'
+import Spinner from './components/Spinner';
+import MovieCard from './components/MovieCard';
+import { useDebounce } from 'react-use';
+import { updateSearchCount } from './appwrite';
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+};
+
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [debounceSearchTerm, setDebounceSearchTerm] = useState(searchTerm);
+
+
+  // Debounce det du skriver i søkefelter for å hindre for mange API-kall mens du skriver
+  useDebounce(
+    () => {
+      setDebounceSearchTerm(searchTerm);
+    },
+    500,
+    [searchTerm]
+  );
+
+  const fetchMovies = async (query = '') => {
+    setIsLoading(false);
+    setErrorMessage('');
+    try { 
+      const endpoint = query
+      ?`${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+      : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      
+      if (data.response === 'False') {
+        setErrorMessage(data.error || 'No movies found.');
+        setMovieList([]);
+        return;
+      }
+      setMovieList(data.results || []);
+
+      updateSearchCount(); // Oppdater søketellingen i Appwrite hver gang en søk utføres
+    } catch (error) {
+      console.error('Error fetching movies:', error);
+      setErrorMessage('Failed to fetch movies. Please try again later.');
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies(debounceSearchTerm);
+  }, [debounceSearchTerm])
+
+  return (
+    <main>
+      <div className='pattern' />
+
+      <div className='wrapper'>
+          <header>
+            <img src="./hero.png" alt="Hero Banner" />
+            <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without The Hassle</h1>
+          </header>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+
+          <section className='all-movies'>
+            <h2 className='mt-[40px]'>All Movies</h2>
+
+            {isLoading ? (
+              <Spinner />
+            ) : errorMessage ? (
+              <p className='text-red-500'>{errorMessage}</p>
+            ) : ( 
+              <ul>
+                {movieList.map((movie) => (
+                  <MovieCard key={movie.id} movie={movie} />
+                ))}
+              </ul>
+            )}
+          </section>
+
+      </div>
+    </main>
+  )
+}
+
+export default App  
